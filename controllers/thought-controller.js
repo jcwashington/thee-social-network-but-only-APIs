f777d0edc8b474eaca62c21d1253c7a2ac7e87aa
JCWashington <jasmine.washington412@gmail.com>
2022-03-12 21:16:15 -0500

    WIP thought controllers and routes stubs

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+const { User, Thought, Reaction } = require('../models');
+
+module.exports = {
+    // get all thoughts
+    getAllThoughts (req, res) {
+
+    },
+    // get thought by _id
+    getThoughtById (req, res) {
+
+    },
+    createThought (req, res) {
+
+    },
+    updateThought (req, res) {
+
+    },
+    deleteThought (req, res) {
+
+    },
+    addReaction (req, res) {
+
+    },
+    removeReaction (req, res) {
+        
+    }
+}
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
 
+// /api/thoughts/
+router.route('/').get(getAllThoughts).post(createThought);
+// /api/thoughts/:thoughtId 
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+// /api/thoughts/:thoughtId/reactions
+router.route('/:id/reactions').post(addReaction);
+// / api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:id/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
