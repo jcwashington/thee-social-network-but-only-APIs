262973c39b00e03c8bcc2e9b324642b161937725
JCWashington <jasmine.washington412@gmail.com>
2022-03-12 19:56:27 -0500

    user controllers and /api/users routes done

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,10 +12,10 @@ module.exports = {
     getUserById(req, res) {
         User.findOne({ _id: req.params.id })
         .select('-__v')
-        .populate([
-            { path: 'thoughts', select: '-__v'},
-            { path: 'friends', select: '-__v'}
-        ])
+        // .populate([
+        //     { path: 'thoughts', select: '-__v'},
+        //     { path: 'friends', select: '-__v'}
+        // ])
         .then((userData) =>
         !userData
             ? res.status(404).json({ message: 'No user found with that ID'})
@@ -30,15 +30,28 @@ module.exports = {
     },
     // Update user by id
     updateUser(req, res) {
-
+        User.findOneAndUpdate({ _id: req.params.id }, { $set: req.body}, { new: true, runValidators: true })
+        .then((userData) =>
+        !userData
+            ? res.status(404).json({ message: 'No user found with that ID'})
+            : res.json(userData))
+        .catch((err) => res.status(500).json(err));
     },
-    // Delete user
+    // Delete user by ID
     deleteUser(req, res) {
-
+        User.findOneAndDelete({ _id: req.params.id })
+        .then((userData) =>
+        !userData
+            ? res.status(404).json({ message: 'No user found with that ID'})
+            // also remove this user from all of the friends lists they are in
+            : User.updateMany({ _id: { $in: userData.friends }}, { $pull: { friends: req.params.id }})
+        )
+        .then(() => res.json({ message: 'User deleted!' }))
+        .catch((err) => res.status(500).json(err));
     },
     // Add a new friend to a user's friend list
     addFriend(req, res) {
-
+        
     },
     // Remove friend from user's friend list
     removeFriend(req, res) {
